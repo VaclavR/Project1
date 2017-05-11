3d37652d0c4e4b11ec117fe9d20dee6ebf2d40e4
vencator <vaclav.rysavy@tutanota.com>
2017-05-11 15:52:58 +0200

    added a few components

--- src/app/recipes/recipe-list/recipe-list.component.ts
+++ src/app/recipes/recipe-list/recipe-list.component.ts
@@ -1,11 +1,19 @@
 import { Component, OnInit } from '@angular/core';
 
+import { Recipe } from '../recipe.model'
+
 @Component({
   selector: 'app-recipe-list',
   templateUrl: './recipe-list.component.html',
   styleUrls: ['./recipe-list.component.css']
 })
 export class RecipeListComponent implements OnInit {
+  recipes: Recipe[] = [
+    new Recipe('A Test Recipe', 'This is simply a test',
+    'http://www.seriouseats.com/images/2015/09/20150914-pressure-cooker-recipes-roundup-09.jpg'),
+    new Recipe('A Test Recipe 2', 'This is simply a test 2',
+    'http://media2.sailusfood.com/wp-content/uploads/2016/03/recipe-of-momos.jpg')
+  ];
 
   constructor() { }
 
